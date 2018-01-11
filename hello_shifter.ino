8f3c49c668a8b12d02ddbbc68a87c8d0f2ced068
RobertBaruch <robert.c.baruch@gmail.com>
2018-01-10 23:41:46 -0800

    Adds example Arduino sketch

--- hello_shifter.ino
+++ hello_shifter.ino
@@ -2,7 +2,7 @@
 
 // Arduino wiring:
 //
-// Nixie board LE to Arduino pin 2
+// Nixie board LE to Arduino pin 4
 // Nixie board OE to Arduino pin 3
 // Nixie board DIN to Arduino pin 11 (aka MOSI)
 // Nixie board CP to Arduino pin 13 (aka SCK)
@@ -13,7 +13,7 @@
 //
 // Also remember to hook up +5V, GND, and +145 - 155 volts.
 
-#define SHIFTER_LE 2
+#define SHIFTER_LE 4
 #define SHIFTER_OE 3
 #define SPI_RATE_HZ 1000000
 #define LE_PULSE_USEC 1
@@ -36,42 +36,43 @@ void setup() {
 // The boolean array must be the same size as the string,
 // and each element is true if you want the dot lit up
 // at that position. Remember that dots are on the left.
+//
+// bit patterns: (note that we transmit byte0, then byte1 (big endian)
+// byte0      --byte1--
+// xxxx x000  0000 0001 : 0
+// xxxx x000  0000 0010 : 1
+// xxxx x000  0000 0100 : 2
+// xxxx x000  0000 1000 : 3
+// xxxx x000  0001 0000 : 4
+// xxxx x000  0010 0000 : 5
+// xxxx x000  0100 0000 : 6
+// xxxx x000  1000 0000 : 7
+// xxxx x001  0000 0000 : 8
+// xxxx x010  0000 0000 : 9
+// xxxx x100  0000 0000 : dot
 
-void writeString(char *str, bool *dots) {
-  int bits = strlen(str) * 12;
-  int bytes = (bits + 7) / 8;
-  
-  uint8_t data[bytes];
-  int index = bytes - 1;
-  
-  for (int i = 0; i < strlen(str); i += 2) {
-    char c = str[i];
+void writeChar(char* str, bool* dots, uint8_t* data) {
+    char c = *str;
     uint16_t b = 0;
     
-    // 0x001 = .
-    // 0x002 = 0
-    // 0x004 = 1 and so on.
+    if (c >= '0' && c <= '9') b = 0x0001 << (c - '0');
+    if (*dots) b |= 0x0400;
 
-    if (c >= '0' && c <= '9') b = 0x002 << (c - '0');
-    if (dots[i]) b |= 0x001;
+    data[0] = b >> 8;
+    data[1] = b & 0xFF;
+}
 
-    data[index] = b >> 4;
-    data[index + 1] = (b << 4) & 0xFF;
-
-    if (i >= strlen(str)) break;
-    
-    c = str[i + 1];
-    b = 0;
-    if (c >= '0' && c <= '9') b = 0x002 << (c - '0');
-    if (dots[i + 1]) b |= 0x001;
-
-    data[index + 1] |= b >> 8;
-    data[index + 2] = b & 0xFF;
-
-    index += 3;
+void writeString(char *str, bool *dots) {
+  int bytes = 2 * strlen(str);
+  uint8_t data[bytes];
+  int index = 0;
+  
+  for (int i = 0; i < strlen(str); ++i) {
+    writeChar(str + i, dots + i, data + index);
+    index += 2;
   }
 
-  for (int i = bytes - 1; i >= 0; i--) {
+  for (int i = 0; i < bytes; ++i) {
     SPI.transfer(data[i]);
   }
 
@@ -82,10 +83,18 @@ void writeString(char *str, bool *dots) {
 
   // Enable output, if it wasn't enabled before.
   digitalWrite(SHIFTER_OE, HIGH);
+  delay(1000);
 }
 
 void loop() {
-  bool dots[] = {false, true, false, false, false, false};
-  writeString("314159", dots);
+  for (int i = 0; i < 10; ++i) {
+    for (int j = 0; j < 10; ++j) {
+      for (int k = 0; k < 10; ++k) {
+        bool dots[] = {false, j%3 == 2, j%3 == 1, j%3 == 0};
+        char data[5] = {' ', '0' + i, '0' + j, '0' + k, 0};
+        writeString(data, dots);
+      }
+    }
+  }
 }
 
